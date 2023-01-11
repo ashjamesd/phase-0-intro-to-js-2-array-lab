eab93c9da060e8f9efda11908de88f129ad803e7
Ash <ashjames.dean@gmail.com>
2023-01-11 16:11:11 -0500

    commit tree 9b8931d0f168672fa5ab93412c2a7ea910ff2176

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution here!
+let cats = ['Milo','Otis','Garfield'];
+function destructivelyAppendCat(name){
+    let pushCats = cats.push(name);
+    return pushCats;
+}
+
+function destructivelyPrependCat(name){
+    let unshiftCats = cats.unshift(name);
+    return unshiftCats;
+}
+
+function destructivelyRemoveLastCat(){
+    let removeLastCat = cats.pop()
+    return removeLastCat;
+}
+
+function destructivelyRemoveFirstCat(){
+    let shiftCat = cats.shift()
+    return shiftCat;
+}
